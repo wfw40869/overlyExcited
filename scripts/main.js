ceaf097602815afa12d1e857459318b845dcb1be
Will Wilkinson <40211746+wfw40869@users.noreply.github.com>
2019-07-15 14:38:53 -0500

    Added functionality to print to console

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,23 @@
-const addExcitement = (sentence) => {
-    console.log(`${sentence}!!!`)
+let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
+
+const addExcitement = (theWordArray) => {
+    let buildMeUp = ""
+
+    for (let i = 0; i < theWordArray.length; i++) {
+        /*
+            If the current word's place (not index) in the array
+            is evenly divisible by 3, add an exclamation point
+            to the end of the word and then concatenate it to
+            `buildMeUp`.
+
+            Otherwise, just concatenate the word itself.
+         */
+        buildMeUp += `${theWordArray[i]} `
+
+        // Print buildMeUp to the console
+        console.log(buildMeUp)
+    }
+    
 }
+
+addExcitement(sentence)
